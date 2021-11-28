a791453c12d03fdcadda7a31023735245bf3c0a7
Telyn Z <175827+telyn@users.noreply.github.com>
2021-11-28 13:24:17 +0000

    sketch stuff in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello world!");
+mod https_url_rewriter;
+
+use https_url_rewriter::url_rewriter;
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World".into()))
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3080));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc)
+                                    .with_graceful_shutdown(shutdown_signal());
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
